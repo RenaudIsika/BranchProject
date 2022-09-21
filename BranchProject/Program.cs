d26837b83a7b2940605747bab8288af64d6b887e
Your Name <you@example.com>
2022-09-21 17:55:35 +0200

    Master World !

--- BranchProject/Program.cs
+++ BranchProject/Program.cs
@@ -2,4 +2,4 @@
 // que je ne suis pas obligé de faire un choix directe
 // entre les deux solutions propoées
 
-﻿Console.WriteLine("Hello, World Master + C!");
+﻿Console.WriteLine("Hello, World Master");
